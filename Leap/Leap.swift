fb64382f1baa99cbf5af8ca61a4141b12ed96766
Damien Burke <damien@Feather.local>
2015-05-20 00:51:50 -0700

    Add passing implementation

--- Leap/Leap.swift
+++ Leap/Leap.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+class Year {
+ 
+    let isLeapYear: Bool
+    
+    init(calendarYear:NSInteger){
+        if (isDivisableBy(calendarYear, 4)) {
+            if (isDivisableBy(calendarYear, 100)) {
+                if (isDivisableBy(calendarYear, 400)) {
+                    isLeapYear = true;
+                } else {
+                    isLeapYear = false
+                }
+            } else {
+                isLeapYear = true
+            }
+        } else {
+            isLeapYear = false
+        }
+    }
+}
+
+func isDivisableBy(number:NSInteger, divisor:NSInteger) -> Bool {
+    return (number % divisor) == 0
+}
