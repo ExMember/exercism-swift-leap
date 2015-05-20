e50cc80dc1bcbdad9d82f0d492da2257b0ae5a59
Damien Burke <damien@Feather.local>
2015-05-20 00:15:30 -0700

    Setup for exercise

---
+++ Leap/Leap.swift
@@ -0,0 +1,9 @@
+//
+//  Leap.swift
+//  Leap
+//
+//  Created by Damien Burke on 5/20/15.
+//  Copyright (c) 2015 Damien Burke. All rights reserved.
+//
+
+import Foundation
--- LeapTests/LeapTests.swift
+++ LeapTests/LeapTests.swift
@@ -1,36 +1,25 @@
-//
-//  LeapTests.swift
-//  LeapTests
-//
-//  Created by Damien Burke on 5/20/15.
-//  Copyright (c) 2015 Damien Burke. All rights reserved.
-//
-
-import UIKit
 import XCTest
 
-class LeapTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+class LeapTest : XCTestCase {
+
+	func testVanillaLeapYear() {
+	  let year = Year(calendarYear: 1996)
+	  XCTAssertTrue(year.isLeapYear)
+	}
+
+	func testAnyOldYear() {
+	  let year = Year(calendarYear: 1997)
+	  XCTAssertTrue(!year.isLeapYear)
+	}
+
+	func testCentury() {
+	  let year = Year(calendarYear: 1900)
+	  XCTAssertTrue(!year.isLeapYear)
+	}
+
+	func testExceptionalCentury() {
+	  let year = Year(calendarYear: 2400)
+	  XCTAssertTrue(year.isLeapYear)
+	}
+
 }
